3b29360dd067f173b683a4cbd4055dc640331479
Fitsumhelina <permin1501424@gmail.com>
2024-06-13 22:40:26 +0300

    from daggy

---
+++ 008-objects.js
@@ -0,0 +1,30 @@
+// object = A collection of related properties and/or methods
+//                Can represent real world objects (people, products, places)
+//                object = {key:value,
+//                               function()}
+
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "lovely",
+    age: 30,
+    isEmployed: true,
+    sayHello: function(){console.log("Hi! I am Spongebob!")},
+    eat: function(){console.log("I am eating your food ")},
+}
+
+const person2 = {
+    firstName: "fitsum",
+    lastName: "helina",
+    age: 42,
+    isEmployed: false,
+    sayHello: () => console.log("Hey, I'm fitsum..."),
+    eat: () => console.log("I am eating roast beef, chicken, and pizza"),
+}
+
+console.log(person1.firstName);
+console.log(person2.firstName);
+
+person1.sayHello();
+person1.eat();
+person2.sayHello();
+person2.eat();
---
+++ 013-events/013-events.js
@@ -0,0 +1,28 @@
+// eventListener = Listen for specific events to create interactive web pages
+//                             events: click, mouseover, mouseout
+//                            .addEventListener(event, callback);
+
+const myBox = document.getElementById("myBox");
+
+myBox.addEventListener("click", event => {
+    event.target.style.backgroundColor = "tomato";
+    event.target.textContent = "OUCH! 🤕";
+});
+
+myBox.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "yellow";
+    event.target.textContent = "Don't do it 😲";
+});
+
+myBox.addEventListener("mouseout", event => {
+    event.target.style.backgroundColor = "lightgreen";
+    event.target.textContent = "Click Me 😀";
+});
+
+document.addEventListener("keydown", event => {
+    myBox.textContent = "Let go, Please 😲";
+    myBox.style.backgroundColor = "tomato";
+});
+
+
+
